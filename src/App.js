572c648326c343242383fa356f0baeb6749e49a4
Nin <nin@Nins-MacBook-Pro.local>
2024-03-30 21:58:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Form from './components/Form/Form.js';
 import BodyMain from './components/BodyMain/Bodymain.js';
-import Admin from './components/admin/admin.js';
-import AddMovieForm from './components/add/add.js';
-import Cart from './components/Cart/Cart.js';
 
 function App() {
-    const [loggedInUser, setLoggedInUser] = useState(null);
-    const [showAdmin, setShowAdmin] = useState(false);
 
-    const handleLoginSuccess = (user) => {
-        setLoggedInUser(user);
-        setShowAdmin(false);
-    };
 
     return (
         <div className="App">
